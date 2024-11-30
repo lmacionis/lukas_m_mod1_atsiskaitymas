554eef213b6a75327a569267aec357be80de127f
lmacionis <lmacionis@yahoo.com>
2024-11-30 14:36:46 +0200

    Implemented test for mthod get_articles, to check if it retuns list

--- lukas_m_mod1_atsiskaitymas/crawl.py
+++ lukas_m_mod1_atsiskaitymas/crawl.py
@@ -1,5 +1,3 @@
-from http.client import responses
-
 import requests
 from lxml.etree import HTML
 from requests import HTTPError
@@ -13,27 +11,30 @@ class Article:
         response = requests.get(self.source)
 
         if response.status_code == 200:
-            try:
-                text = response.text
-                tree = HTML(text)
-                articles = tree.xpath("//div[contains(@class, 'col-span-12 lg:col-span')]")
-                return articles
-            except ValueError:
-                ...
+            text = response.text    # str
+            tree = HTML(text)
+            articles = tree.xpath("//div[contains(@class, 'col-span-12 lg:col-span')]")
+            return articles
         else:
             raise HTTPError(f"Page is unreachable: {response.status_code}")
 
     def get_subject(self):
-        time_limit = 60
+        articles_list = [self.get_articles()]
         subject_list = []
-        for article in self.get_articles():
+
+        for article in articles_list[0]:
             subject = article.xpath(".//h2/a/text()")[0].strip()
             subject_list.append(subject)
+        print(subject_list)
         return subject_list
 
-    def get_article_url(self, url=None):
+    def get_article_url(self):
+        articles_list = [self.get_articles()]
         url_list = []
-        for link in self.get_articles():
+
+        for link in articles_list[0]:
             url = link.xpath(".//a/@href")[0]
             url_list.append(url)
+
+        print(url_list)
         return url_list
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -1,18 +1,30 @@
 import  unittest
 from requests import HTTPError
-
 from lukas_m_mod1_atsiskaitymas.crawl import Article
 from unittest.mock import patch
+
 
 class TestCrawl(unittest.TestCase):
     @patch("requests.get")
 
-    def test_get_articles(self, mock_get):
+    # Testing if response code is 200 if not raise error
+    def test_get_articles_response(self, mock_get):
         article = Article("www.example.com")
-        mock_get.return_value.status_code = 400
+        mock_get.return_value.status_code = 404
 
         with self.assertRaises(HTTPError):
             article.get_articles()
+
+    # ToDo Need to fix this
+    @patch("requests.Response")
+    def test_get_articles_data(self, mock_response):
+        mock_response.return_value.text = 1
+        data = Article("https://example.com")
+
+    # Testing if return value is list
+    def test_get_articles(self):
+        data = Article("https://example.com")
+        assert data.get_articles() == []
 
 
 if __name__ == "__main__":
